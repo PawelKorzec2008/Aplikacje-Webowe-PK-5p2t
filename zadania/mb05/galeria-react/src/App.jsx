9edab2d439d87d2fb008b47a8f3710a4b2732740
Pawel Korzec <pawel8korzec@gmail.com>
2026-09-22 11:10:38 +0200

    MB5: komponent Footer

--- zadania/mb05/galeria-react/src/App.jsx
+++ zadania/mb05/galeria-react/src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
 import Navbar from '../components/Navbar'
 import CategoryBar from '../components/CategoryBar'
 import Gallery from '../components/Gallery'
+import Footer from '../components/Footer'
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
           <CategoryBar/>
           <Gallery/>
         </main>
+        <Footer/>
       </div>
     </>
   )
